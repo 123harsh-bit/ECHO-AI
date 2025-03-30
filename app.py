d09c4007689627625de94cca0c35542ad5193afc
123harsh-bit <memersofbsc@gmail.com>
2025-03-30 23:34:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ migrate = Migrate(app, db)  # Enable Flask-Migrate for migrations
 
 # ------------------ DATABASE MODELS ------------------
 
-class User(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
